8a6eecaf7e36b1f8a34aec9b2e487f1fd29c2ceb
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 10:31:09 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import * as c from './components/index';
 export default function Home() {
   return (
     <main className="w-full min-h-full">
-      <c.Greet text="World" />
+      <c.Greet text="Jello, World!" />
     </main>
   );
 }
