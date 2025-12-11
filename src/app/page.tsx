52241c8aa826c27ec0e9caeef785219ad9e2ebc4
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 10:00:23 -0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import * as c from './components/index';
+
 export default function Home() {
   return (
     <main className="w-full min-h-full">
-      <p className="text-9xl mx-auto text-center">Jello, world!</p>
+      <Greet text="World" />
     </main>
   );
 }
