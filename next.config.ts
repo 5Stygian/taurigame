729a8b32d5c93cc62a340c883c59a358ad4ee515
5Stygian <rlcrafistohard@gmail.com>
2025-12-10 22:23:50 -0800

    set default tauri configs

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+const internalHost = process.env.TAURI_DEV_HOST || 'localhost';
+
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+
+  /* tauri configs */
+  output: 'export',
+  assetPrefix: isProd ? undefined : `http://${internalHost}:3000`,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
