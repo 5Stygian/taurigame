af2fc8b4902d12f124467e09e5bd525b32980b43
5Stygian <121655781+5Stygian@users.noreply.github.com>
2025-12-11 10:08:25 -0800

    Update greet.tsx

--- src/app/components/misc/greet.tsx
+++ src/app/components/misc/greet.tsx
@@ -1,4 +1,8 @@
-/** this is a test to see if IPC works on a browser */
+/**
+This is a test to see if IPC works on a web deployment
+
+Status: it doesnt work
+*/
 
 'use client'
 
@@ -10,13 +14,15 @@ interface GreetProps {
 }
 
 export default function Greet({ text }: GreetProps) {
+  /*
   const [text, setText] = useState<string>('');
 
   useEffect(() => {
         invoke<string>('greet', { greet: {text} }).then((result) => {
             setText(result);
         });
-    }, []);
+  }, []);
+  */
   
   return (
     <span className="text-9xl mx-auto text-center">{text}</span>
